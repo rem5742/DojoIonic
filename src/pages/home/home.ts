e9dcb8c2e156f3716386b58a63618d0a6241080e
David  J <rem5742@gmail.com>
2017-08-26 10:29:28 -0500

    Initial Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { GithubProvider } from '../../providers/github/github';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  informacion:any = []
+  username:string = '';
+  constructor(private _github:GithubProvider) {
+  }
 
+  fnDojo()
+  {
+    if (!this.username)
+    {
+      alert('Nombre de usuario vacio');
+      return;
+    }
+    var promise = this._github.buscarUsuario(this.username)
+    promise.then(data=>{
+      this.informacion = data
+    });
   }
 
 }
